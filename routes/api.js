85dedfc204256f15d58143a5d27c53a271e2bfcf
Tarun Boddupalli <tarunbod@gmail.com>
2017-08-02 22:10:58 -0400

    simplify code, fix error of responding twice

--- routes/api.js
+++ routes/api.js
@@ -39,19 +39,18 @@ router.post('/register', function(req, res) {
                             success: false,
                             message: err.message
                         });
+                    } else {
+                        res.json({
+                            success: true,
+                            message: "Registration successful. Check email."
+                        });
                     }
-
-                    res.json({
-                        success: true,
-                        message: "Registration successful. Check email."
-                    });
                 });
             }
         }
     });
-
-    
 });
+
 router.post('/authenticate', function(req, res) {
     Student.findOne({
         email: req.body.email
@@ -61,8 +60,8 @@ router.post('/authenticate', function(req, res) {
                 success: false,
                 message: "Authentication failed. User not found."
             });
-        } else if(user) {
-            if(user.password != req.body.password) {
+        } else {
+            if(user.password !== req.body.password) {
                 res.json({
                     success: false,
                     message: "Authentication failed. Wrong password."
@@ -77,14 +76,13 @@ router.post('/authenticate', function(req, res) {
                     message: "Enjoy token!",
                     token: token
                 });
-
             }
         }
     });
 });
 
 
-router.get('/student', function(req, res, next) {
+router.get('/students', function(req, res, next) {
     Student.find(function(err, students) {
         if (err) {
             next(err);
@@ -95,14 +93,12 @@ router.get('/student', function(req, res, next) {
             });
         }
     });
-
-    
 });
 
-router.get('/similar', function(req, res) {
-    // 5980c7a55e359d104130ae41
-    var id = req.body.id;
-});
+// router.get('/similar', function(req, res) {
+//     // 5980c7a55e359d104130ae41
+//     var id = req.body.id;
+// });
 
 function getRandomId() {
     var dict = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -115,121 +111,7 @@ function getRandomId() {
     return dict[num1] + dict[num2] + dict[num3] + dict[num4] + dict[num5] + dict[num6];
 }
 
-/*
-router.get('/create', function(req, res) {
-    var s1 = new Student();
-    s1.name = 'Tarun Boddupalli';
-    s1.grade = 11;
-    s1.classes.push({
-        id: 'E1',
-        period: '1',
-        teacher: 'Fedowitz, Elizabeth',
-        room: '195'
-    }, {
-        id: 'E2',
-        period: '2',
-        teacher: 'Nasser, Gene',
-        room: '235'
-    }, {
-        id: 'E3',
-        period: '3',
-        teacher: 'DiOrio, Jennifer',
-        room: '135'
-    }, {
-        id: 'E4',
-        period: '4/5',
-        teacher: 'Morrow, Denise',
-        room: '140'
-    }, {
-        id: 'E000',
-        period: '6',
-        teacher: 'Lunch',
-        room: 'CAFE'
-    }, {
-        id: 'E6',
-        period: '7/8',
-        teacher: 'Hesse, LeeAnne',
-        room: '215'
-    }, {
-        id: 'E7',
-        period: '9/10',
-        teacher: 'Connell, Robin',
-        room: '254'
-    }, {
-        id: 'E8',
-        period: '11',
-        teacher: 'Malanga, Something',
-        room: '192'
-    }, {
-        id: 'E9',
-        period: '12',
-        teacher: 'Leghorn, Brittany',
-        room: '007'
-    });
-    s1.markModified('classes');
-    s1.save();
-
-    var s2 = new Student();
-    s2.name = 'Bharddwaj Vemulapalli';
-    s2.grade = 11;
-    s2.classes.push({
-        id: 'E11',
-        period: '1',
-        teacher: 'Evans, Michael',
-        room: '252'
-    }, {
-        id: 'E13',
-        period: '2',
-        teacher: 'Other, Teacher',
-        room: '113'
-    }, {
-        id: 'E14',
-        period: '3',
-        teacher: 'Some Other, Teacher',
-        room: '130'
-    }, {
-        id: 'E12',
-        period: '4/5',
-        teacher: 'Stocker, Focker',
-        room: '236'
-    }, {
-        id: 'E000',
-        period: '6',
-        teacher: 'Lunch',
-        room: 'CAFE'
-    }, {
-        id: 'E6',
-        period: '7/8',
-        teacher: 'Hesse, LeeAnne',
-        room: '215'
-    }, {
-        id: 'E7',
-        period: '9/10',
-        teacher: 'Connell, Robin',
-        room: '254'
-    }, {
-        id: 'E8',
-        period: '11',
-        teacher: 'Malanga, Something',
-        room: '192'
-    }, {
-        id: 'E10',
-        period: '12',
-        teacher: 'Frey, Diane',
-        room: '227'
-    });
-    s2.markModified('classes');
-    s2.save();
-
-    res.json({
-        success: true
-    });
-});
-*/
-
 //this has to be at the bottom
-
-
 router.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
