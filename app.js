dbd88b2929202effe0a5d533cb31470bc09b7e6a
IshanArya <ishan.arya130@gmail.com>
2017-08-24 15:09:00 -0400

    Removed https (less secure now-- he he)

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req, res, next) {
-    if(req.protocol === 'http') {
-        res.redirect("https://" + req.get('host') + req.originalUrl);
-    } else {
-        next();
-    }
-});
+// app.use(function(req, res, next) {
+//     if(req.protocol === 'http') {
+//         res.redirect("https://" + req.get('host') + req.originalUrl);
+//     } else {
+//         next();
+//     }
+// });
 
 
 app.use('/api', api);
