2abcf295e796e8af85adbb8400f7f6fee10c37a0
IshanArya <ishan.arya130@gmail.com>
2017-08-24 17:00:41 -0400

    Added early release option and programmed for end user illiteracy

--- public/js/register.js
+++ public/js/register.js
@@ -4,22 +4,26 @@ $(function() {
 		// console.log("press");
 		if($('#email').val() && $('#password1').val() && $('#name').val()) {
 			if($('#password1').val() === $('#password2').val()) {
-				$.post("/api/register", {
-					email: $('#email').val().trim(),
-					password: $('#password1').val(),
-					name: $('#name').val().trim()
-				}, function(data) {
-					if(data.success) {
-						window.location.assign('/registered');
-					} else {
-						console.log(data.message);
-						if(data.message === "Email already in use.") {
-							notify("Email already in use. Please use a different one.");
+				if ($('#name').val().split(" ").length > 1) {
+					$.post("/api/register", {
+						email: $('#email').val().trim(),
+						password: $('#password1').val(),
+						name: $('#name').val().trim()
+					}, function(data) {
+						if(data.success) {
+							window.location.assign('/registered');
 						} else {
-							notify('Server error. Please report this @ <a href="mailto:cindrinc@gmail.com" target="_blank">cindrinc@gmail.com</a>');
+							console.log(data.message);
+							if(data.message === "Email already in use.") {
+								notify("Email already in use. Please use a different one.");
+							} else {
+								notify('Server error. Please report this @ <a href="mailto:cindrinc@gmail.com" target="_blank">cindrinc@gmail.com</a>');
+							}
 						}
-					}
-				});
+					});
+				} else {
+					notify("Please enter a full name.");
+				}
 			} else {
 				notify("Passwords do not match.");
 			}
